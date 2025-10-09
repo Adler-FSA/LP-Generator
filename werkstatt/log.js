f0559391d24e1bc04583047d362ece900bace632
Adler-FSA <info@finanziellesouveraenitaet.de>
2025-10-09 22:36:01 +0200

    log.js

---
+++ werkstatt/log.js
@@ -0,0 +1,9 @@
+const log = (() => {
+  const container = document.getElementById("slot-log");
+  function write(msg){
+    const time = new Date().toLocaleTimeString();
+    container.innerHTML += `<div>[${time}] ${msg}</div>`;
+  }
+  function clear(){ container.innerHTML = ""; }
+  return { write, clear };
+})();
