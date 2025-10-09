71e6263e57df9fdc911ee3f6c526d4e5f37465ff
Adler-FSA <info@finanziellesouveraenitaet.de>
2025-10-09 22:23:42 +0200

    modules.js

---
+++ poststelle/js/modules.js
@@ -0,0 +1,15 @@
+(function(){
+  const el = document.getElementById("slot-modules");
+  el.innerHTML = `
+    <h3>📦 Letzte Commits (Repo)</h3>
+    <ul style="list-style:none;padding:0;margin:0;font-size:13px;color:#9db4d6;">
+      <li>🚀 Test-Commit via Poststelle</li>
+      <li>✨ Initial commit</li>
+    </ul>
+    <h3 style="margin-top:20px;">🛠️ Shortcuts & Info</h3>
+    <ul style="list-style:none;padding:0;margin:0;font-size:13px;color:#9db4d6;">
+      <li><a href="https://github.com/Adler-FSA" target="_blank">📂 Repo öffnen</a></li>
+      <li><a href="#" target="_blank">🌐 Live Seite</a></li>
+    </ul>
+  `;
+})();
